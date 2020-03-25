81c458dd748d45f0bf2a0de3e38d464e8fedafa6
Joseph Schlitz <jrschlitz0725@gmail.com>
2020-03-24 21:05:45 -0400

    Creating an individual class for the MyInfo Component

---
+++ src/components/MyInfo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function MyInfo() {
+  return (
+    <div>
+      <h1 id="header1">Joseph Schlitz</h1>
+      <p>
+        I am a software engineer at a small telecommunications company. Soon I
+        will be a software engineer 2 at the Space Telescope Science Institute.
+      </p>
+      <ul>
+        <li>Japan</li>
+        <li>Germany</li>
+        <li>Spain</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-import App from "./App";
+import MyInfo from "./components/MyInfo.js";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 
-function MyInfo() {
-  return (
-    <div>
-      <h1 id="header1">Joseph Schlitz</h1>
-      <p>
-        I am a software engineer at a small telecommunications company. Soon I
-        will be a software engineer 2 at the Space Telescope Science Institute.
-      </p>
-      <ul>
-        <li>Japan</li>
-        <li>Germany</li>
-        <li>Spain</li>
-      </ul>
-    </div>
-  );
-}
 ReactDom.render(<MyInfo />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
