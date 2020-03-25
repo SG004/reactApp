f6b22e91bf23f690be210abcd2f7d4bce390d401
Joseph Schlitz <jrschlitz0725@gmail.com>
2020-03-25 00:22:53 -0400

    Followed from 29:43-50:53 in the tutorial. Learned about the creation of separate files for components and nesting components.

---
+++ src/childParentPractice/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar.js";
+import MainContent from "./MainContent.js";
+import Footer from "./Footer.js";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/childParentPractice/MainContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main>
+      <p>
+        This is where the main content will go. I do not know what i should do
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
